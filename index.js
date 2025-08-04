69ecf496096390675ab9f88249e9ea1813a058c6
tired_ <173179578+thetireddude@users.noreply.github.com>
2025-08-04 14:28:41 -0400

    fix: add new todos to ul list & li styling

--- index.js
+++ index.js
@@ -5,31 +5,44 @@ const todos = []
 
 const test = document.getElementById('test')
 
+const todoList = document.querySelector('ul.todo-list')
+
+
+
 loadJSON()
 
 addBtn.addEventListener('click', addTodo)
 
 function addTodo() {
     const text = input.value
-    test.innerHTML += `<h1>${text}</h1>`
+    // test.innerHTML += `<h1>${text}</h1>`
+    todoList.innerHTML += `
+        <li class="todo">
+            <input type="checkbox" id="${text}">
+            <label for="${text}">${text}</label>
+        </li>
+    `
     input.value = ''
 
     todos.push(text)
     storeJSON()
-
 }
-
 
 function loadJSON() {
     const json = localStorage.getItem('todos')
-    console.log(json)
+    console.log(`stored json:\n ${json}`)
 
     if (json!= null) {
-        console.log('true')
         const list = JSON.parse(json)
 
         for (const x of list){
-            test.innerHTML += `<h1>${x}</h1>`
+            // test.innerHTML += `<h1>${x}</h1>`
+            todoList.innerHTML += `
+                <li class="todo">
+                    <input type="checkbox" id="${x}">
+                    <label for="${x}">${x}</label>
+                </li>
+            `
             todos.push(x)
         }
     }
@@ -38,5 +51,6 @@ function loadJSON() {
 function storeJSON() {
     const json = JSON.stringify(todos)
     localStorage.setItem('todos', json)
+    console.log(json)
 }
 
